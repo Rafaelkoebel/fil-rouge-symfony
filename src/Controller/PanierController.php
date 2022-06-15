ca6987616126d767105e9a253c7c5f8bd5dce54c
Rafaelkoebel <rafael.koebel@gmail.com>
2022-06-15 18:08:00 +0200

    panier finalisé

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -16,6 +16,8 @@ class PanierController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(SessionInterface $session, VenteRepository $venteRepository): Response
     {
+        // $fruiti = $ventes->getutilisateur()->getUserIdentifier();
+        // dd($fruiti);
         $panier = $session->get("panier", []);
 
         // On "fabrique" les données
@@ -31,7 +33,13 @@ class PanierController extends AbstractController
             $total += $vente->getPrix() * $quantite;
         }
 
-        return $this->render('panier/index.html.twig', compact("dataPanier", "total"));
+        // return $this->render('panier/index.html.twig', compact("dataPanier", "total"));
+
+        return $this->render('panier/index.html.twig', [
+            'dataPanier' => $dataPanier,
+            'total' => $total,
+            // 'vente' => $ventes,
+        ]);
 
     }
 
@@ -42,23 +50,65 @@ class PanierController extends AbstractController
 
         $panier = $session->get('panier', []);
         $id = $vente->getId();
-        // $fruiti = $vente->getUtilisateur();
-        // dd($vente);
-        if (!empty($panier[$id])) {
-            $panier[$id]++;
-        }
-        else {
+
+        // recup les id des utilisateurs , des utilisateurs de vente/annonce //
+        $fruitivente = $vente->getUtilisateur()->getId();
+
+        
+        // quantite de LA vente dans le panier //
+        // dd($panier[$fruiti]);
+
+        // id vente => quantite //
+        // dd($panier);
+
+        // id fruiticulteur(utilisateur) //
+        // dd($fruitivente);
+
+
+
+        // Si le panier est rempli, que l'id utilisateur dans le panier est diff de l'id de l'utilisateur de la vente //
+
+        //Si le panier est vide
+        if (empty($panier)) {
             $panier[$id] = 1;
         }
+        else {
+            //On récup le fruiti de la session
+            $fruitipanier = $session->get('fruiti');
+            //Si le fruiti de la session != fruitivente  -> message d'erreur
+            if ($fruitipanier != $fruitivente) {
+                $this->addFlash('refuse', 'Le fournisseur est différent, veuillez passer commande puis renouvelez votre action.');
+                return $this->redirectToRoute('app_annonce_apercut');
+            }
+            //Si la vente = fruitipanier
+            //Si la vente est déja dans la panier
+            if (!empty($panier[$id])) {
+                $panier[$id]++;
+            }
+            else {
+                //Ajoute la ligne au panier
+                $panier[$id] = 1;
+            }
+                
+        }
+
+
+        // if (!empty($panier[$id])) {
+        //     $panier[$id]++;
+        // }
+        // else {
+        //     $panier[$id] = 1;
+        // }
 
         // if ($utilisateur != $panier[$utilisateur]) {
         //     $panier->remove($utilisateur);
         // }
 
         //On sauvegarde dans la session
-        
+        $session->set('fruiti', $fruitivente);
         $session->set('panier', $panier);
 
+        //  dd($session);
         return $this->redirectToRoute('app_panier_index');
 
     }
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Utilisateur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +13,18 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
-        $this->getUser()->setRoles(["ROLE_FRUITICULTEUR"]);
-        dd($this->getUser()->getRoles());
+        // $nom = $form->handleRequest($request);
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        ]);
+    }
+
+    #[Route('/test2', name: 'app_test2')]
+    public function index2(Request $request): Response
+    {
+        dd($request);
+        // $nom = $form->handleRequest($request);
+        return $this->render('test/index2.html.twig', [
+            // 'form' => $nom,
         ]);
     }
 }
